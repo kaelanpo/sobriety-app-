a6f4ffd12b6e05c0cf6ea501afff130ca68cc004
COMEDI.AI <your.email@example.com>
2025-09-19 13:09:00 -0700

    Initial Commit

---
+++ Test Cursor/Test_CursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_CursorApp.swift
+//  Test Cursor
+//
+//  Created by MacBook on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Test_CursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
